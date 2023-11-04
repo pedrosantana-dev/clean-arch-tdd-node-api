963200a674f4f76c710077a377314cc0963cf3a5
Pedro Santana <pedromanu27@gmail.com>
2023-11-04 17:06:23 -0300

    garante que EmailValidator chama validator com email  correto

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -22,4 +22,11 @@ describe('Email Validator', () => {
     const isEmailValid = sut.isValid('invalid_email@email.com')
     expect(isEmailValid).toBe(false)
   })
+
+  test('Should call validator with correct email', () => {
+    validator.isEmailValid = false
+    const sut = makeSut()
+    sut.isValid('any_email@email.com')
+    expect(validator.email).toBe('any_email@email.com')
+  })
 })
